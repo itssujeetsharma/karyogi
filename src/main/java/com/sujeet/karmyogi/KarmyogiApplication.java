1082e053408d1e086f5f8f60de7a271b252ce88d
ersujeet <sujeetsharma9341@gmail.com>
2022-09-09 21:52:31 +0530

    first commit

---
+++ src/main/java/com/sujeet/karmyogi/KarmyogiApplication.java
@@ -0,0 +1,13 @@
+package com.sujeet.karmyogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarmyogiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarmyogiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sujeet/karmyogi/KarmyogiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujeet.karmyogi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarmyogiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
